c16dbee509b3b4a709be5dae5f3b1604629584bf
eeghor <eeghor@gmail.com>
2017-03-26 11:17:37 +1100

    seems fine

--- gmaps-pc-driving-distances.py
+++ gmaps-pc-driving-distances.py
@@ -10,46 +10,57 @@ d["destinations"] = d["sub"] + ' ' + d["state"] + ' ' + d["postcode"].map(str)
 
 dest_lst = d["destinations"].tolist()
 
+N_PCODES = len(dest_lst)
+N_DIST = N_PCODES*(N_PCODES - 1) - sum(range(1, N_PCODES + 1) - 1)
+
 gmaps = googlemaps.Client(key="AIzaSyCsJnOb6VESNe9C-BXpkbrLppPA2ygCJMg")
 
-TOTAL = len(dest_lst) - 1
 DAILY_CMAX = 2500
-SOFAR = 0
+ORIG_DONE = 0
 SOFAR_THIS_ORG = 0
-TOTAL_THIS_ORG = TOTAL  # initially, all other postcodes but the last one
 
+org = dest_lst.pop()
+TOCOLLECT = N_PCODES - 1  # initially, all other postcodes but the last one
+		
 # function to collect distances; we'd like to run it with the scheduler
 def collect_distances():
 
-	if SOFAR < TOTAL:  # not all distances collected yet
-	
-		if SOFAR_THIS_ORG + DAILY_CMAX < TOTAL_THIS_ORG:  # then keep collection for this origin
-			org = dest_lst.pop()
-			dest = dest_lst[SOFAR_THIS_ORG:SOFAR_THIS_ORG + DAILY_CMAX]	
-			res = googlemaps.distance_matrix(origins=org, destinations=dest, units="metric", mode="driving")
-			SOFAR_THIS_ORG += DAILY_CMAX
-			SOFAR += DAILY_CMAX  # say started from 0, now 2500
-		else: 
-			NXTORG = TOTAL_THIS_ORG - (SOFAR_THIS_ORG + DAILY_CMAX)  # this many can do for the next origin
-			dest = dest_lst[SOFAR_THIS_ORG:TOTAL_THIS_ORG]  # finish the rest for this origin
-			res = googlemaps.distance_matrix(origins=org, destinations=dest, units="metric", mode="driving")
-			# switch to the next origin
-			SOFAR += (TOTAL_THIS_ORG - SOFAR_THIS_ORG)
-			# reser sofar for the nex torigin
-			SOFAR_THIS_ORG = 0
-			org = dest_lst.pop()
-			TOTAL_THIS_ORG = len(dest_lst)
+	global ORIG_DONE, SOFAR_THIS_ORG, TOCOLLECT
+
+	if ORIG_DONE < N_DIST:  # not all origina done yet
 			
-			dest = dest_lst[SOFAR_THIS_ORG:SOFAR_THIS_ORG + NXTORG]
+		if SOFAR_THIS_ORG + DAILY_CMAX < TOCOLLECT:  # then keep collection for this origin
+			
+			dest = dest_lst[SOFAR_THIS_ORG:SOFAR_THIS_ORG + DAILY_CMAX]
+			
 			res = googlemaps.distance_matrix(origins=org, destinations=dest, units="metric", mode="driving")
-		return SOFAR
+
+			SOFAR_THIS_ORG += DAILY_CMAX
+
+		else: 
+			
+			dest = dest_lst[SOFAR_THIS_ORG:TOCOLLECT]  # finish the rest for this origin
+			
+			res = googlemaps.distance_matrix(origins=org, destinations=dest, units="metric", mode="driving")
+			
+			# switch to the next origin
+			ORIG_DONE += 1
+			
+			org = dest_lst.pop()
+			SOFAR_THIS_ORG = 0
+			TOCOLLECT = len(dest_lst) - 1
+			
+			dest = dest_lst[SOFAR_THIS_ORG: TOCOLLECT - DAILY_CMAX]
+			res = googlemaps.distance_matrix(origins=org, destinations=dest, units="metric", mode="driving")
+
+		return ORIG_DONE
+	
 	else:
 
 		return -1
 
-	p = dest_lst[-1]
-	# schedule.every().day.at("10:30").do(collect_distances)
+schedule.every().day.at("10:30").do(collect_distances)
 
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
